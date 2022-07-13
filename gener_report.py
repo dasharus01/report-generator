d7e2a9ceb94c80b7962fa425947e019dcd1f98ea
Dasha <rusackowadasha@yandex.ru>
2022-07-13 20:51:04 +0300

    Новый код

--- gener_report.py
+++ gener_report.py
@@ -6,153 +6,57 @@ import sys
 
 from docx import Document
 from docx.shared import Inches
-def generator1(founder, name, faculty, department,namestudent, group, ids, studbilet):
-    doc = DocxTemplate("template.docx")
-    context = { 'founder' : founder, 
-                'name' : name,
-                'faculty' : faculty,
-                'department' : department,
-                'namestudent' : namestudent,
-                'group' : group,
-                'ids' : ids,
-                'studbilet' : studbilet}
-    doc.render(context)
-    doc.save("report.docx")
+import argparse
+import json
+parser = argparse.ArgumentParser()
 
-    
-    
-# основная программа
+parser.add_argument('-o', '--org', help="Info about organisation")
+parser.add_argument('-s', '--stu', help="Info about student")
+parser.add_argument('-r', '--rep' , help = "Info about report")
+parser.add_argument('-t', '--tea' , help = "Info about teacher")
+#parser.add_argument('--struc' , help = "Info about paragraph")
 # год текущий
 now = datetime.datetime.now()
 year = now.year 
-# заголовок
-# founder = sys.argv[1]
-# name = sys.argv[2]
-i = 2
-j = 0
-founder = ""
-name = ""
-faculty = ""
-department = ""
-namestudent = ""
-group = ""
-ids = ""
-studbilet = ""
-k = len (sys.argv)
-if sys.argv[1] == "МИНИСТЕРСТВО": 
-    founder  = sys.argv[1]
-    
-    while sys.argv[i]!= ",":
-        founder = founder + " " + sys.argv[i]
-        i = i + 1
-    i = i + 1
-    if  i < k:
-        if sys.argv[i] == "федеральное" or sys.argv[i] == "ГУАП":
-            while sys.argv[i]!= ",":
-                name = name + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1
-    if  i < k:
-        if sys.argv[i] == "ИНСТИТУТ":
-            while sys.argv[i]!= ",":
-                faculty = faculty + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1
-    if  i < k:
-        if sys.argv[i] == "КАФЕДРА":
-            while sys.argv[i]!= ",":
-                department = department + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1
-
-if sys.argv[1] == "федеральное" or sys.argv[1] == "ГУАП":
-    name = sys.argv[1]
-    while sys.argv[i]!= ",":
-        name = name + " " + sys.argv[i]
-        i = i + 1
-    i = i + 1
-    if  i < k:
-        if sys.argv[i] == "ИНСТИТУТ":
-            while sys.argv[i]!= ",":
-                faculty = faculty + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1
-    if  i < k:
-        if sys.argv[i] == "КАФЕДРА":
-            while sys.argv[i]!= ",":
-                department = department + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1
-if sys.argv[1] == "ИНСТИТУТ":
-    faculty = sys.argv[1]
-    while sys.argv[i]!= ",":
-        faculty = faculty + " " + sys.argv[i]
-        i = i + 1
-    i = i + 1
-    if  i < k:
-        if sys.argv[i] == "КАФЕДРА":
-            while sys.argv[i]!= ",":
-                department = department + " " + sys.argv[i]
-                i = i + 1
-            i = i + 1  
-if sys.argv[1] == "КАФЕДРА":
-    department = sys.argv[1]
-    while sys.argv[i]!= ",":
-        department = department + " " + sys.argv[i]
-        i = i + 1
-    i = i + 1              
-if department == "":
-    print("Кафедра обязательна!")
-    sys.exit()
-if founder == "" and name == "":
-    founder = "МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ"
-    name = "федеральное государственное автономное образовательное учреждение высшего образования «САНКТ-ПЕТЕРБУРГСКИЙ ГОСУДАРСТВЕННЫЙ УНИВЕРСИТЕТ АЭРОКОСМИЧЕСКОГО ПРИБОРОСТРОЕНИЯ»"
-namestudent = sys.argv[i] + " " 
-i = i+1
-j = 0 
-while sys.argv[i] != ",":
-    if sys.argv[i] != " " and j == 0:
-        namestudent = namestudent  + sys.argv[i][0] + "."
-        j = j + 1
-    elif sys.argv[i] != " " and j != 0:
-        namestudent = namestudent  + sys.argv[i][0] 
-    i = i+1
-i = i + 1 
-group = sys.argv[i]
-i = i + 2
-j = 0;
-#ids = sys.argv[i]
-while j < len(sys.argv[i]):
-    if sys.argv[i][j] == "/":
-        ids = sys.argv[i]
+def pr (d):
+    doc = DocxTemplate('template.docx')
+    #преобразуем имя студента
+    if d['student']['patronymic'] != "":
+        names = d['student']['surname'] + " " + d['student']['name'][0] + "." + d['student']['patronymic'][0] + "."
+    else:
+        names = d['student']['surname'] + " " + d['student']['name'][0] + "."
+    if d['teacher']['patronymic'] != "":
+        teachern = d['teacher']['surname'] + " " + d['teacher']['name'][0] + "." + d['teacher']['patronymic'][0] + "."
+    else:
+        teachern = d['teacher']['surname'] + " " + d['teacher']['name'][0] + "."
+    studbilet = ""
+    if d['student']['identity_card'] != "":
         studbilet = "Студенческий билет №"
-    j = j + 1
-i = i +1 
-generator1(founder, name, faculty, department,namestudent, group, ids, studbilet)
+    context = {'founder' : d['organisation']['founder'],
+               'name' : d['organisation']['name'],
+               'faculty' : d['organisation']['faculty'],
+               'department' : d['organisation']['department'],
+               'namestudent' : names,
+               'group' : d['student']['group'],
+               'ids' : d['student']['identity_card'],
+               'studbilet' : studbilet,
+               'typework' : d['report']['task_type'],
+               'namework' : d['report']['task_name'],
+               'subject' : d['report']['subject_name'],
+               'status' : d['teacher']['status'],
+               'teachername' : teachern,
+               'year' : year
+              }
+    doc.render(context)
+    doc.save('report.docx')
 
+d = {}
+args = parser.parse_args()
+d["organisation"] = json.loads(args.org)
+d["student"] = json.loads(args.stu)
+d["report"] = json.loads(args.rep)
+d["teacher"] = json.loads(args.tea)
+#d["report_structure"] = json.loads(args.struc)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#print(len(d["report_structure"]))
+pr(d)
